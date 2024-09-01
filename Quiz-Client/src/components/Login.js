8bb19e94677764a3a3083c3765768094509ce1c9
Lobna Abd El-Aziz <90930843+LobnaMuhamed@users.noreply.github.com>
2024-09-02 00:10:48 +0300

    Upload Quiz-Client Side

---
+++ Quiz-Client/src/components/Login.js
@@ -0,0 +1,101 @@
+import {
+  TextField,
+  Button,
+  Box,
+  Card,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import React, {useEffect} from "react";
+import Center from "./Center";
+import useForm from "../hooks/useForm";
+import { createAPIEndpoint, ENDPOINTS } from "../API";
+import useStateContext from "../hooks/useStateContext";
+import { useNavigate } from "react-router-dom";
+
+const getFreshModel = () => ({
+  name: "",
+  email: "",
+});
+export default function Login() {
+  const { context, setContext, resetContext } = useStateContext();
+  const navigate = useNavigate();
+  const { values, setValues, errors, setErrors, handleInputChange } =
+    useForm(getFreshModel);
+
+
+  useEffect(() => {
+    resetContext();
+  }, []);  
+  const login = (e) => {
+    e.preventDefault();
+    if (validate())
+      createAPIEndpoint(ENDPOINTS.participant)
+        .post(values)
+        .then((res) => {
+          setContext({
+            participantId: res.data.participantId,
+            timeTaken: 0,
+            selectedOptions: [],
+          });
+          navigate("/quiz");
+        })
+        .catch((err) => console.log(err));
+  };
+  const validate = () => {
+    let temp = {};
+    temp.email = /\S+@\S+\.\S+/.test(values.email) ? "" : "Email is not valid";
+    temp.name = values.name !== "" ? "" : "This field is required";
+    setErrors(temp);
+    return Object.values(temp).every((x) => x === "");
+  };
+  return (
+    <Center>
+      <Card sx={{ width: 400 }}>
+        <CardContent sx={{ textAlign: "center" }}>
+          <Typography variant="h3" sx={{ my: 3 }}>
+            Quizz App
+          </Typography>
+          <Box
+            sx={{
+              "& .MuiTextField-root	": {
+                m: 1,
+                width: "90%",
+              },
+            }}
+          >
+            <form noValidate autoComplete="on" onSubmit={login}>
+              <TextField
+                label="Email"
+                name="email"
+                variant="outlined"
+                value={values.email}
+                onChange={handleInputChange}
+                {...(errors.email && { error: true, helperText: errors.email })}
+              />
+              <TextField
+                label="Name"
+                name="name"
+                variant="outlined"
+                value={values.name}
+                onChange={handleInputChange}
+                {...(errors.name && { error: true, helperText: errors.name })}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                size="large"
+                sx={{
+                  width: "90%",
+                }}
+                disableRipple
+              >
+                Start
+              </Button>
+            </form>
+          </Box>
+        </CardContent>
+      </Card>
+    </Center>
+  );
+}
